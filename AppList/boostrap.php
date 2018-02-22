eec8337797cb88246ab36cd73eba96dac520fd96
viadd <maycol_19.8@hotmail.com>
2018-02-22 16:09:04 -0500

    Applist

---
+++ AppList/boostrap.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+
+	
+		
+
+	<div class="container-fluid">
+		<div class="row">
+		  <div class="col-sm-4 col-lg-3">.col-sm-3</div>
+		  <div class="col-sm-2 col-lg-3">.col-sm-3</div>
+		  <div class="col-sm-4 col-lg-3">.col-sm-3</div>
+		  <div class="col-sm-2 col-lg-3">.col-sm-3</div>
+		
+		
+		</div>
+	</div>
+
+</body>
+</html>
